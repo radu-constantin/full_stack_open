13a042779e6cfb25d78f4469eaa0ba62a778eda2
Radu <raduadrian.constantin94@gmail.com>
2023-02-20 20:20:36 +0200

    Deployed to fly.io

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -14,6 +14,10 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint"});
 };
 
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 app.use(requestLogger);
 
@@ -91,7 +95,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })

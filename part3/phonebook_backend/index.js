d416bc44450665bc89177d058a83321c8aa98a12
Radu <raduadrian.constantin94@gmail.com>
2023-02-21 08:01:55 +0200

    Removed morgan

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
-const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-// app.use(morgan('tiny'));
-morgan.token('body', function (req, res) {
-  return JSON.stringify(req.body);
-});
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   {

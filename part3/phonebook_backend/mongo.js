83421108e2b16dbe34a699aeccd86a0744c64918
Radu <raduadrian.constantin94@gmail.com>
2023-02-22 19:37:33 +0200

    Added exercise 3.12

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('Please enter a password!');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://radu:${password}@cluster0.ugozdx9.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person);
+    })
+    mongoose.connection.close();
+  })
+} else {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const newPerson = new Person({
+    name,
+    number
+  });
+
+  newPerson.save().then(result => {
+    console.log(`Added ${name} number: ${number} to phonebook!`);
+    mongoose.connection.close();
+  })
+}
+
+
+

3cdbed355dee27b8aaa1388511753daa1695da1b
Radu <raduadrian.constantin94@gmail.com>
2023-02-28 20:17:54 +0200

    Added exercise 4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -16,7 +16,25 @@ function totalLikes(blogs) {
   }, 0);
 }
 
+function favoriteBlog(blogs) {
+  let favoriteBlog = null;
+  blogs.forEach(blog => {
+    if (favoriteBlog === null) {
+      favoriteBlog = blog;
+    } else if (blog.likes > favoriteBlog.likes) {
+      favoriteBlog = blog;
+    }
+  });
+
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes
+  };
+}
+
 module.exports={
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };

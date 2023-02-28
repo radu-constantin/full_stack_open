141c2b9e447a39fff4ed7e92007f7a5ffe90657c
Radu <raduadrian.constantin94@gmail.com>
2023-02-28 20:04:02 +0200

    Added exercise 4.4

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,7 +1,22 @@
+const { mapReduce } = require("../models/blog");
+
 function dummy(blogs) {
   return 1;
+};
+
+function totalLikes(blogs) {
+  let likeList = [];
+
+  blogs.forEach(blog => {
+    likeList.push(blog.likes);
+  });
+
+  return likeList.reduce((total, value) =>{
+    return total + value;
+  }, 0);
 }
 
 module.exports={
   dummy,
+  totalLikes
 };

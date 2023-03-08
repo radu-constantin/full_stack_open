da02ff4e50c67cb4124bf93199dcb851d22371f6
Radu <raduadrian.constantin94@gmail.com>
2023-03-08 20:53:53 +0200

    Added users test

--- part3/backend/tests/test_helper.js
+++ part3/backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Note = require('../models/note');
+const User = require('../models/user');
 
 const initialNotes = [
   {
@@ -13,11 +14,8 @@ const initialNotes = [
 
 const nonExistingId = async () => {
   const note = new Note({ content: 'willremovethissoon' });
-  console.log(note);
   await note.save();
   await note.remove();
-
-  console.log(note);
 
   return note._id.toString();
 };
@@ -27,6 +25,11 @@ const notesInDb = async () => {
   return notes.map(note => note.toJSON());
 };
 
+const usersInDb = async() => {
+  const users = await User.find({});
+  return users.map(u => u.toJSON());
+};
+
 module.exports = {
-  initialNotes, nonExistingId, notesInDb
+  initialNotes, nonExistingId, notesInDb, usersInDb
 };

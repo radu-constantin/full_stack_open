fb39a3904db1eb89c993d4c7112b496291897854
Radu <raduadrian.constantin94@gmail.com>
2023-03-12 19:42:47 +0200

    Added exercise 4.20

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -7,14 +7,15 @@ const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const middleware = require('./utils/middleware');
 const config = require('./utils/config');
-
 
 mongoose.set("strictQuery", false);
 mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.tokenExtractor);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);

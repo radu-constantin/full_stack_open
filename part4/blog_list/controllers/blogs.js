9420bce04c8408717b3a14bff08bffe0479e7439
Radu <raduadrian.constantin94@gmail.com>
2023-03-12 19:59:23 +0200

    Added exercise 4.21

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -5,14 +5,6 @@ const blogsRouter = express.Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-// const getTokenFrom = request => {
-//   const authorization = request.get('authorization');
-//   if (authorization && authorization.startsWith('Bearer ')) {
-//     return authorization.replace('Bearer ', '');
-//   }
-//   return null;
-// };
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1});
 
@@ -43,7 +35,19 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid'});
+  }
+
+  const user = await User.findById(decodedToken.id);
+  const blog = await Blog.findById(request.params.id);
+
+  if (user.id === blog.user.toString()) {
+    blog.deleteOne();
+  } else {
+    response.status(403).json({error: 'user is not owner of blog'});
+  }
 
   response.status(204).end();
 });

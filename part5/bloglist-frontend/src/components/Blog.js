d94af2a632b4487b9a6b1b991cce0ef4d26f4763
Radu <raduadrian.constantin94@gmail.com>
2023-03-27 09:35:46 +0300

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,31 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+
+const blogStyle = {
+  border: "1px solid black",
+  padding: "5px",
+  marginBottom: "10px"
+}
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const condensedBlog = (
+    <div >
+      {blog.title} {blog.author}<button onClick={() => setShowDetails(true)}>view</button>
+    </div>
+  )
+
+  const expandedBlog = (
+    <div style={blogStyle}>
+      <div>{blog.title} {blog.author} <button onClick={() => setShowDetails(false)}>hide</button></div>
+      <div>{blog.url}</div>
+      <div>likes 0 <button>like</button></div>
+    </div>
+  )
+
+  return (
+    showDetails ? expandedBlog : condensedBlog
+  )
+}
 
 export default Blog

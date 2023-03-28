84912b4cd102d6e902ea05fbba5a1323a16106f7
Radu <raduadrian.constantin94@gmail.com>
2023-03-28 06:40:29 +0300

    Added exercise 5.10

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -1,10 +1,23 @@
 import Blog from './Blog';
 
+function sortBlogs(blogA, blogB) {
+  console.log(blogA);
+  console.log(blogB);
+  if (blogA.likes > blogB.likes) {
+    return -1;
+  } else if (blogA.likes < blogB.likes) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 function BlogList({ blogs }) {
+  const sortedBlogs = blogs.sort(sortBlogs)
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {sortedBlogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
